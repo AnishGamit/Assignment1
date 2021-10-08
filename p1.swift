c6d99bb5b2e7b6313c55e878fe5fd076aadfa7d5
Anishgamit <89537119+AnishGamit@users.noreply.github.com>
2021-10-08 18:43:57 +0000

    done

---
+++ p1.swift
@@ -0,0 +1,27 @@
+enum Myerror: Error{
+  case negative
+
+  var msg: String{
+    switch self{
+      case .negative: return "ERROR: Number cannot be negative"
+    }
+  }
+}
+func fun(n: Int) throws{
+  if(n<0){
+    throw Myerror.negative
+  }
+  if(n%2==0){
+    print("n is even")
+  }
+  else{
+    print("n is odd")
+  }
+}
+do{
+  try fun(n: 55)
+} catch{
+  if let error = error as? Myerror{
+    print(error.msg)
+  }
+  }
